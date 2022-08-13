23703625470f88497907e9a7fdf95e9801cc018a
Abdulbari Qaisar <Abdulbari.112917@gmail.com>
2022-08-13 14:15:17 +0300

    handling errors

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ export default function Login() {
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -10, opacity: 0 }}
       transition={{ duration: 0.2 }}
-      className={` flex  md:mx-24 md:flex-wrap  lg:flex-nowrap py-10 lg:py-0   lg:px-5 justify-start flex-col md:flex-row  items-center min-h-screen`}
+      className={` flex  md:mx-24 md:flex-wrap  lg:flex-nowrap py-10 lg:py-0   lg:px-5 justify-center flex-col md:flex-row  items-start min-h-screen`}
     >
       <LoginForm></LoginForm>
     </motion.div>
