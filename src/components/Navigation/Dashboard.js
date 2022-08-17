6925bae54b6b44da0ed27772f7e5ca72db612ec7
Abdulbari <abdulbari.112917@gmail.com>
2022-08-18 00:13:52 +0300

    Adding style to the dashboard

--- src/components/Navigation/Dashboard.js
+++ src/components/Navigation/Dashboard.js
@@ -4,14 +4,33 @@ import ToRead from "../../pages/bookshelves/ToRead";
 import HaveRead from "../../pages/bookshelves/HaveRead";
 import Favorite from "../../pages/bookshelves/Favorite";
 import ReadingNow from "../../pages/bookshelves/ReadingNow";
+import useTheme from "../../hook/useTheme";
 export default function Dashboard() {
+  const { color, text, mode } = useTheme();
   return (
-    <div className="flex flex-col gap-20 text-red-300">
-      <Link to="/bookshelves/favorite">Favorite</Link>
-      <Link to="/bookshelves/toread">To Read</Link>
+    <div
+      className={`flex ${
+        mode === "bg-gray-900" ? "text-white" : "text-gray-900"
+      } flex-col gap-20  border-r-2 font-light pr-4 text-2xl`}
+    >
+      <Link className={`flex items-center gap-2 `} to="/bookshelves/favorite">
+        <ion-icon name="heart-outline"></ion-icon>
+        Favorite
+      </Link>
 
-      <Link to="/bookshelves/readingnow">readingnow</Link>
-      <Link to="/bookshelves/haveread">Have Read</Link>
+      <Link className={`flex items-center gap-2 `} to="/bookshelves/toread">
+        <ion-icon name="today-outline"></ion-icon>
+        To Read
+      </Link>
+
+      <Link className={`flex items-center gap-2 `} to="/bookshelves/readingnow">
+        <ion-icon name="flask-outline"></ion-icon>
+        Reading now
+      </Link>
+      <Link className={`flex items-center gap-2 `} to="/bookshelves/haveread">
+        <ion-icon name="checkmark-outline"></ion-icon>
+        Have Read
+      </Link>
     </div>
   );
 }
